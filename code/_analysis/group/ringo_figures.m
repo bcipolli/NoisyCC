42f5789f74eca0147fb62963a7286763bc0553f7
Ben Cipollini <bcipolli@ucsd.edu>
2014-08-13 11:37:45 -0700

    Rename function to access standard figures.
    Former-commit-id: cfba1d41cf165e11400e936abb255435fb1fa600

--- code/_analysis/group/ringo_figures.m
+++ code/_analysis/group/ringo_figures.m
@@ -120,33 +120,55 @@ function f = plot_hu_sim(cdata, ndata, lbl)
 
     f = figure;
 
-    cdata_rh = cdata.intact.rh_sim;
-    cdata_lh = cdata.intact.lh_sim;
-    ndata_rh = ndata.intact.rh_sim;
-    ndata_lh = ndata.intact.lh_sim;
+    cdata_rh = cdata.lesion.rh_sim;
+    cdata_lh = cdata.lesion.lh_sim;
+    ndata_rh = ndata.lesion.rh_sim;
+    ndata_lh = ndata.lesion.lh_sim;
 
-    subplot(2,2,1);
-    imagesc(pdist2mat(mean(cdata_rh)));
+    subplot(2,3,1);
+    imagesc(pdist2mat(mean(cdata_rh, 1)));
+    axis square;
     set(gca, 'xtick',[],'ytick',[]);
     title(['(no-noise) RH Similarity' lbl]);
 
-    subplot(2,2,2);
-    imagesc(pdist2mat(mean(cdata_lh)));
+    subplot(2,3,4);
+    imagesc(pdist2mat(mean(cdata_lh, 1)));
+    axis square;
     set(gca, 'xtick',[],'ytick',[]);
     title(['(no-noise) LH Similarity' lbl]);
 
-    subplot(2,2,3);
-    imagesc(pdist2mat(mean(ndata_rh)));
+    subplot(2,3,2);
+    imagesc(pdist2mat(mean(ndata_rh, 1)));
+    axis square;
     set(gca, 'xtick',[],'ytick',[]);
     title(['(noise) RH Similarity' lbl]);
 
-    subplot(2,2,4);
-    imagesc(pdist2mat(mean(ndata_lh)));
+    subplot(2,3,5);
+    imagesc(pdist2mat(mean(ndata_lh, 1)));
+    axis square;
     set(gca, 'xtick',[],'ytick',[]);
     xlabel('LH pattern #');
     title(['(noise) LH Similarity' lbl]);
 
+    if isfield(cdata, 'rh_in_sim')
+        subplot(2,3,3);
+        imagesc(pdist2mat(cdata.rh_in_sim));
+        axis square;
+        set(gca, 'xtick',[],'ytick',[]);
+        title('input similarity');
+
+        subplot(2,3,6);
+        imagesc(pdist2mat(cdata.rh_out_sim));
+        axis square;
+        set(gca, 'xtick',[],'ytick',[]);
+        title('output similarity');
+    end;
+    
+    
+    % take the dot product for each, then take the mean of the result.
+    no_nan = @(d) (d(~isnan(d)));
     mean_dotted = @(d1, d2) ((mean(sum(d1 .* d2, 2) ./ sqrt(sum(d1.^2, 2)) ./ sqrt(sum(d2.^2, 2)))));
+    mean_dotted_nan = @(d1, d2) (mean_dotted(no_nan(d1), no_nan(d2)));
     shared_count = min(size(cdata_lh,1), size(ndata_lh, 1));
 
     if (numel(cdata_rh) > 0)
@@ -154,18 +176,29 @@ function f = plot_hu_sim(cdata, ndata, lbl)
         fprintf('   noise LH-RH similarity (%s): %f\n', lbl, mean_dotted(ndata_rh, ndata_lh));
         fprintf('no/noise LH-LH similarity (%s): %f\n', lbl, mean_dotted(cdata_lh(1:shared_count), ndata_lh(1:shared_count)));
         fprintf('no/noise RH-RH similarity (%s): %f\n', lbl, mean_dotted(cdata_rh(1:shared_count), ndata_rh(1:shared_count)));
+
+        if isfield(cdata, 'rh_in_sim')
+            %keyboard
+
+            cdata_rh_vs_input = cdata_rh - repmat(cdata.rh_in_sim, [size(cdata_rh, 1) 1]);
+            cdata_lh_vs_input = cdata_lh - repmat(cdata.lh_in_sim, [size(cdata_lh, 1) 1]);
+            ndata_rh_vs_input = ndata_rh - repmat(ndata.rh_in_sim, [size(ndata_rh, 1) 1]);
+            ndata_lh_vs_input = ndata_lh - repmat(ndata.lh_in_sim, [size(ndata_lh, 1) 1]);
+
+            fprintf('\nVS INPUT\n');
+            fprintf('no-noise LH-RH similarity (%s): %f\n', lbl, mean_dotted_nan(cdata_rh_vs_input, cdata_lh_vs_input));
+            fprintf('   noise LH-RH similarity (%s): %f\n', lbl, mean_dotted_nan(ndata_rh_vs_input, ndata_lh_vs_input));
+
+            cdata_rh_vs_output = cdata_rh - repmat(cdata.rh_out_sim, [size(cdata_rh, 1) 1]);
+            cdata_lh_vs_output = cdata_lh - repmat(cdata.lh_out_sim, [size(cdata_lh, 1) 1]);
+            ndata_rh_vs_output = ndata_rh - repmat(ndata.rh_out_sim, [size(ndata_rh, 1) 1]);
+            ndata_lh_vs_output = ndata_lh - repmat(ndata.lh_out_sim, [size(ndata_lh, 1) 1]);
+
+            fprintf('\nVS OUTPUT\n');
+            fprintf('no-noise LH-RH similarity (%s): %f\n', lbl, mean_dotted_nan(cdata_rh_vs_output, cdata_lh_vs_output));
+            fprintf('   noise LH-RH similarity (%s): %f\n', lbl, mean_dotted_nan(ndata_rh_vs_output, ndata_lh_vs_output));
+        end;
     end;
-
-function mat = pdist2mat(dat)
-    sz =  (1 + sqrt(1 + 4*1*2*numel(dat))) / 2;  % quadratic solution for M(M-1)/2 = numel(dat)
-    guru_assert(sz == floor(sz), 'sz is an integer.');
-
-    lower =  double(~triu(ones(sz)));
-    upper = double(~~triu(ones(sz)) - eye(sz));
-
-    lower(logical(lower)) = dat;
-    upper(logical(upper)) = dat;
-    mat = lower + upper;
 
 
 function f = plot_hu_sim2(cdata, ndata, lbl)
