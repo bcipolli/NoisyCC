bcab1494a47ed2a1e62ef3bcaf2dc44d2a0c7c8d
Ben Cipollini <bcipolli@ucsd.edu>
2014-10-01 16:53:19 -0700

    Loop over individual models, to spread across parameter search first and run multiple examples second.
    Former-commit-id: 1b01ee4c5e29b1fafadfb2ddc95dcfa7287d205d

--- scripts/asymmetry/asymmetry_looper.m
+++ scripts/asymmetry/asymmetry_looper.m
@@ -13,49 +13,58 @@ function [nets, pats, datas, figs] = asymmetry_looper(net, nexamples, nccs, dela
     sims = cell(size(nets));
     simstats = cell(size(nets));
 
-    for ni = 1:length(nccs), for di=1:length(delays), for ti=1:length(Ts)
+    rseed = net.sets.rseed;
+    
+    for mi=1:nexamples, for ni = 1:length(nccs), for di=1:length(delays), for ti=1:length(Ts)
+        if mi==1
+            nets{ni, di, ti} = cell(nexamples, 1);
+            datas{ni, di, ti} = cell(nexamples, 1);
+        end;
 
         % set params
         net.sets.ncc = nccs(ni);
         net.sets.D_CC_INIT(:) = delays(di);
         net.sets.T_INIT(:) = Ts(ti) * net.sets.dt;
         net.sets.T_LIM(:) = Ts(ti) * net.sets.dt;
+        net.sets.rseed = net.sets.rseed + (mi-1);
         
         % Train the network
-        [nets{ni, di, ti}, pats, datas{ni, di, ti}] = r_looper(net, nexamples); % run 25 network instances
+        [nets{ni, di, ti}{mi}, pats, datas{ni, di, ti}{mi}] = r_looper(net, 1); % run 25 network instances
 
         % Gather any missing data
-        for xi=1:length(datas{ni, di, ti})
-            if ~isfield(datas{ni, di, ti}{xi}, 'an') || ~isfield(datas{ni, di, ti}{xi}.an, 'sim')
-                net = nets{ni, di, ti}{xi};
-                data = datas{ni, di, ti}{xi};
+        if mi==nexamples
+            for xi=1:length(datas{ni, di, ti})
+                if ~isfield(datas{ni, di, ti}{xi}, 'an') || ~isfield(datas{ni, di, ti}{xi}.an, 'sim')
+                    net = nets{ni, di, ti}{xi};
+                    data = datas{ni, di, ti}{xi};
 
-                % Will propagate data to cell array.
-                fprintf('Computing similarity...')
-                [data.an.sim, data.an.simstats] = r_compute_similarity(net, pats);
-                datas{ni, di, ti}{xi} = data;
+                    % Will propagate data to cell array.
+                    fprintf('Computing similarity...')
+                    [data.an.sim, data.an.simstats] = r_compute_similarity(net, pats);
+                    datas{ni, di, ti}{xi} = data;
 
-                % Hack to make things work A LOT FASTER
-                outfile = fullfile(net.sets.dirname, net.sets.matfile);
-                fprintf(' re-saving to %s ...', outfile);
-                save(outfile,'net','pats','data');
-                fprintf(' done.\n');
+                    % Hack to make things work A LOT FASTER
+                    outfile = fullfile(net.sets.dirname, net.sets.matfile);
+                    fprintf(' re-saving to %s ...', outfile);
+                    save(outfile,'net','pats','data');
+                    fprintf(' done.\n');
+                end;
+            end;
+
+            % Combine the results
+            %anz      = cellfun(@(obj) guru_getfield(obj, 'an', struct()), datas{ni, di, ti}, 'UniformOutput', false);
+            anz                  = cellfun(@(d) d.an, datas{ni, di, ti}, 'UniformOutput', false);
+            sims{ni, di, ti}     = cellfun(@(an) an.sim, anz, 'UniformOutput', false);
+            simstats{ni, di, ti} = cellfun(@(an) an.simstats, anz, 'UniformOutput', false);
+            simstats{ni, di, ti} = mean(cat(5, simstats{ni, di, ti}{:}), 5);
+            clear('anz');
+
+            % Plot results
+            if ~isempty(loop_figs)
+                r_analyze_similarity(nets{ni, di, ti}, sims{ni, di, ti}, simstats{ni, di, ti}, loop_figs);
             end;
         end;
-
-        % Combine the results
-        %anz      = cellfun(@(obj) guru_getfield(obj, 'an', struct()), datas{ni, di, ti}, 'UniformOutput', false);
-        anz                  = cellfun(@(d) d.an, datas{ni, di, ti}, 'UniformOutput', false);
-        sims{ni, di, ti}     = cellfun(@(an) an.sim, anz, 'UniformOutput', false);
-        simstats{ni, di, ti} = cellfun(@(an) an.simstats, anz, 'UniformOutput', false);
-        simstats{ni, di, ti} = mean(cat(5, simstats{ni, di, ti}{:}), 5);
-        clear('anz');
-
-        % Plot results
-        if ~isempty(loop_figs)
-            r_analyze_similarity(nets{ni, di, ti}, sims{ni, di, ti}, simstats{ni, di, ti}, loop_figs);
-        end;
-    end; end; end;
+    end; end; end; end;
 
     if ~isempty(summary_figs)
         r_analyze_similarity_surfaces(nets, sims, simstats, summary_figs);
