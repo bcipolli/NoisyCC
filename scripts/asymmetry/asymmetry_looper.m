0ceb78ba46be1431c781c34c36fbe42eb0807b0d
Ben Cipollini <bcipolli@ucsd.edu>
2014-10-01 16:06:51 -0700

    Compute stats more strategically, allow specifying figs, be more robust.
    Former-commit-id: d0fac67ed7899b1df01b36d74ef9ee3c67c08fc1

--- scripts/asymmetry/asymmetry_looper.m
+++ scripts/asymmetry/asymmetry_looper.m
@@ -1,37 +1,56 @@
-function [nets, pats, datas, figs] = asymmetry_looper(net, nexamples, nccs, delays, Ts)
+function [nets, pats, datas, figs] = asymmetry_looper(net, nexamples, nccs, delays, Ts, loop_figs, summary_figs)
 
-if ~exist('nexamples', 'var'), nexamples = 10; end;
-if ~exist('nccs', 'var'), nccs = [net.sets.ncc]; end;
-if ~exist('delays', 'var'), delays = unique(net.sets.D_CC_INIT); end;
-if ~exist('Ts', 'var'), Ts = unique(net.sets.T_INIT) / net.sets.dt; end;
+    if ~exist('nexamples', 'var'), nexamples = 10; end;
+    if ~exist('nccs', 'var'), nccs = [net.sets.ncc]; end;
+    if ~exist('delays', 'var'), delays = unique(net.sets.D_CC_INIT); end;
+    if ~exist('Ts', 'var'), Ts = unique(net.sets.T_INIT) / net.sets.dt; end;
+    if ~exist('loop_figs', 'var'), loop_figs = []; end;
 
+    nets = cell(length(nccs), length(delays), length(Ts)); 
+    datas = cell(size(nets));
+    sims = cell(size(nets));
+    simstats = cell(size(nets));
 
-nets = cell(length(nccs), length(delays), length(Ts)); 
-datas = cell(size(nets));
-for ni = 1:length(nccs), for di=1:length(delays), for ti=1:length(Ts)
-    % set params
-    net.sets.ncc = nccs(ni);
-    net.sets.D_CC_INIT(:) = delays(di);
-    net.sets.T_INIT(:) = Ts(ti) * net.sets.dt;
-    net.sets.T_LIM(:) = Ts(ti) * net.sets.dt;
+    for ni = 1:length(nccs), for di=1:length(delays), for ti=1:length(Ts)
 
-    net.sets
+        % set params
+        net.sets.ncc = nccs(ni);
+        net.sets.D_CC_INIT(:) = delays(di);
+        net.sets.T_INIT(:) = Ts(ti) * net.sets.dt;
+        net.sets.T_LIM(:) = Ts(ti) * net.sets.dt;
+        
+        % Train the network
+        [nets{ni, di, ti}, pats, datas{ni, di, ti}] = r_looper(net, nexamples); % run 25 network instances
 
-    % Train the network
-    [nets{ni, di, ti}, pats, datas{ni, di, ti}] = r_looper(net, nexamples); % run 25 network instances
-    
-    % Gather any missing data
-    if ~isfield(datas{ni, di, ti}, 'an') || ~isfield(datas{ni, di, ti}.an, 'sim')
-        [datas{ni, di, ti}.an.sim, ...
-         datas{ni, di, ti}.an.simstats] = r_compute_similarity(nets{ni, di, ti}, pats);
-    end;
-    
-    % Plot results
-    r_make_movie_similarity(nets{ni, di, ti}, ...
-        pats, ...
-        datas{ni, di, ti}.an.sim, ...
-        datas{ni, di, ti}.an.simstats, ...
-        '', ...
-        [1 2 4]);
-    keyboard
-end; end; end;
+        % Gather any missing data
+        for xi=1:length(datas{ni, di, ti})
+            if ~isfield(datas{ni, di, ti}{xi}, 'an') || ~isfield(datas{ni, di, ti}{xi}.an, 'sim')
+                net = nets{ni, di, ti}{xi};
+                data = datas{ni, di, ti}{xi};
+
+                % Will propagate data to cell array.
+                fprintf('Computing similarity...')
+                [data.an.sim, data.an.simstats] = r_compute_similarity(net, pats);
+                datas{ni, di, ti}{xi} = data;
+
+                % Hack to make things work A LOT FASTER
+                outfile = fullfile(net.sets.dirname, net.sets.matfile);
+                fprintf(' re-saving to %s ...', outfile);
+                save(outfile,'net','pats','data');
+                fprintf(' done.\n');
+            end;
+        end;
+
+        % Combine the results
+        %anz      = cellfun(@(obj) guru_getfield(obj, 'an', struct()), datas{ni, di, ti}, 'UniformOutput', false);
+        anz                  = cellfun(@(d) d.an, datas{ni, di, ti}, 'UniformOutput', false);
+        sims{ni, di, ti}     = cellfun(@(an) an.sim, anz, 'UniformOutput', false);
+        simstats{ni, di, ti} = cellfun(@(an) an.simstats, anz, 'UniformOutput', false);
+        simstats{ni, di, ti} = mean(cat(5, simstats{ni, di, ti}{:}), 5);
+        clear('anz');
+
+        % Plot results
+        if ~isempty(loop_figs)
+            r_analyze_similarity(nets{ni, di, ti}, sims{ni, di, ti}, simstats{ni, di, ti}, loop_figs);
+        end;
+    end; end; end;
