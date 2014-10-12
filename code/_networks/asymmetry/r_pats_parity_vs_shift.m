d9871e7489dd212f4bde9db910679fd3cad0725b
Ben Cipollini <bcipolli@ucsd.edu>
2014-10-11 18:53:42 -0700

    Fixing bugs in pattern creation... oops\!
    Former-commit-id: aafa90365056417b943f2e31bbf2ac8acb6f0587

--- code/_networks/asymmetry/r_pats_parity_vs_shift.m
+++ code/_networks/asymmetry/r_pats_parity_vs_shift.m
@@ -13,8 +13,8 @@ function [in_pats, out_pats, pat_cls, pat_lbls, idx] = r_pats_parity_vs_shift(se
     in_pats = [ p_in_pats(:, 1:end/2) -1*ones(p_npats, 1) s_in_pats(:, (1+end/2):end)  1*ones(s_npats, 1);
                 s_in_pats(:, 1:end/2)  1*ones(s_npats, 1) p_in_pats(:, (1+end/2):end) -1*ones(p_npats, 1) ];
 
-    out_pats = [p_out_pats(:, 1:end/2)       s_out_pats(:, 1:end/2);
-                s_out_pats(:, (1+end/2):end) p_out_pats(:, (1+end/2):end)];
+    out_pats = [p_out_pats(:, 1:end/2) s_out_pats(:, (1+end/2):end);
+                s_out_pats(:, 1:end/2) p_out_pats(:, (1+end/2):end)];
     pat_cls  = [ ones(size(p_pat_cls)); 2*ones(size(s_pat_cls))];
     pat_lbls = { repmat('P vs S', size(p_pat_lbls)); repmat('S vs P', size(s_pat_lbls)) };
     idx      = []; % use default symmetric labeling
