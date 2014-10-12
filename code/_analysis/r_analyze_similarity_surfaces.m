b662113931c7c596f20eca2574e26236b0c58f27
Ben Cipollini <bcipolli@ucsd.edu>
2014-10-11 18:56:04 -0700

    Do proper weighting across hidden units to get asymmetry across all, in all cases.
    Former-commit-id: 52347ade9b29a1febca1a2968334df0cfb630475

--- code/_analysis/r_analyze_similarity_surfaces.m
+++ code/_analysis/r_analyze_similarity_surfaces.m
@@ -10,7 +10,7 @@ function r_analyze_similarity_surfaces(nets, sims, simstats, figs)
 
     switch ndims_looped
         case 1, r_analyze_similarity_surfaces_1D(nets, sims, simstats, figs, dims{dims_looped});
-        case 2, r_analyze_similarity_surfaces_1D(nets, sims, simstats, figs);
+        case 2, fprintf('2D looping movie NYI\n');%r_analyze_similarity_surfaces_1D(nets, sims, simstats, figs);
         otherwise, error('NYI');
     end;
 
@@ -38,31 +38,49 @@ function r_analyze_similarity_surfaces_1D(nets, sims, simstats, figs, dim)
 % When we vary on just one dimension, show as a surface plot vs. time
 
     vals = compute_common_vals(nets, sims);
+    yvals = sort(vals.(dim));
 
     %% Figure 1: mean difference from output similarity
     if ismember(1, figs)
         for pti=1:vals.npattypes
-            f1h = figure('Position', [ 72         -21        1209         794]);
+            f1h = figure('Position', [ 72         -21        1200         700]);
 
             data = zeros(vals.nlocs, length(vals.(dim)), vals.tsteps);
 
-            for li=2:vals.nlocs+1
-                subplot(2,2,li-1);%1, nlocs, li)
-                set(gca, 'FontSize', 14);
+            for li=2:vals.nlocs+1 % skip input
+                subplot(2,2,li-1);%1, nlocs, li);
+                set(gca, 'FontSize', 16);
 
                 if li <= vals.nlocs
                     for xi=1:length(vals.(dim))
-                        data(li, xi, :) = squeeze(simstats{xi}(:, li, pti, 5));
+                        data(li, xi, :) = abs(squeeze(simstats{xi}(:, li, pti, 5))); %eliminate the sign for cleaner plotting
                     end;
                     surf(1:vals.tsteps, vals.(dim), squeeze(data(li, :, :)));
-                    title(strrep(vals.locs{li}, '_', '\_'));
+
+                    switch vals.locs{li}
+                        case 'cc', tit='inter-';
+                        case 'ih', tit='intra-';
+                        otherwise, tit = vals.locs{li};
+                    end;
+                    title(strrep(tit, '_', '\_'));
                 else
-                    alpha = nets{li}{1}.sets.ncc / nets{li}{1}.sets.nhidden_per;
-                    surf(1:vals.tsteps, vals.(dim), squeeze(alpha * data(2, :, :) + (1-alpha) * data(3, :, :)))
-                    title('intra, combined (avg)');
+                    alphas = cellfun(@(nets) nets{1}.sets.ncc / nets{1}.sets.nhidden_per, nets);
+                    alphas = repmat(alphas(:), [1 vals.tsteps]);
+                    cc_data = squeeze(data(2, :, :)); cc_data(isnan(cc_data)) = 0;
+                    ih_data = squeeze(data(3, :, :)); ih_data(isnan(ih_data)) = 0;
+                    surf_data = alphas .* cc_data + (1-alphas) .* ih_data;
+                    surf(1:vals.tsteps, yvals, surf_data);
+                    title('all hidden');
                 end;
+                set(gca, 'xlim', [1 vals.tsteps], 'ylim', [min(yvals), max(yvals)], 'zlim', sort([0 1]));
+                set(gca, 'ytick', yvals);
                 view([40.5 32]);
-                ylabel(dim); xlabel('tsteps'); zlabel('asymmetry');
+
+                xlabel('tsteps'); zlabel('asymmetry');
+                switch (dim)
+                    case 'ncc', ylabel('# inter- units');
+                    otherwise, ylabel(dim);
+                end;
             end;
         end;
     end;
