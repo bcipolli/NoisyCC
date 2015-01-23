73c9bf3cb242165f18bf98571326ab425986e21a
Ben Cipollini <bcipolli@ucsd.edu>
2015-01-23 11:02:13 -0800

    Adding / cleaning scripts.
    Former-commit-id: 0a06df6a3270a237f91212da7d0876754003f22f

--- scripts/COGSCI2015/lewis_elman_paritypaired_negonly.m
+++ scripts/COGSCI2015/lewis_elman_paritypaired_negonly.m
@@ -3,9 +3,10 @@
 
 net = lewis_elman_common_args();
 
-% Do paired parity task
-net.sets.dataset = 'parity';
+% Do this with negative-only callosal connections.
+net.sets.dataset   = 'parity';  % parallel single-hemisphere tasks
 net.sets.dirname = fullfile(net.sets.dirname, net.sets.dataset);
+net.sets.cc_wt_lim = [-inf 0]; % Constrain CC weights to non-positive
 
 % Manually-determined training parameters
 net.sets.eta_w = 0.005;
@@ -13,8 +14,7 @@ net.sets.phi_w = 0.5;
 net.sets.lambda_w = 1E-3;
 
 % Train over all combinations of ncc and delays
-ncc    = linspace(net.sets.nhidden_per, 0, 6);
+ncc = linspace(net.sets.nhidden_per, 0, 6);
 delays = [1 5 10 15 20];
 
-r_train_and_analyze_all(net, 10, ncc, delays);
-
+r_train_and_analyze_all(net, 25, ncc, delays);
