5c2d695a560b11120abd7178d24816bce21e3c4a
Ben Cipollini <bcipolli@ucsd.edu>
2015-01-31 15:24:58 -0800

    Experiment one: old networks with new (RSA, cross-correlation) analyses
    Former-commit-id: f79194002a77b0387b531dda2af27180197d8b2f

---
+++ scripts/COGSCI2015/experiment_one_nonoise.m
@@ -0,0 +1,12 @@
+clear globals variables;
+addpath(genpath(fullfile(fileparts(which(mfilename)), '..', '..', 'code')));
+dbstop if error;
+
+net = experiment_one_args();
+net.sets.niters = 5000;
+net.sets.axon_noise = 0;
+net.sets.dirname    = fullfile(net.sets.dirname, mfilename);
+
+r_train_and_analyze_many(net, 25); % run 25 network instances
+
+
