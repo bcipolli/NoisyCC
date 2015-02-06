5f136255a330f134aa40a13ff21076ada87a27ee
bcipolli <bcipolli@ucsd.edu>
2015-02-06 07:21:51 -0800

    Removing _lib; need to replace for compat reasons.

--- code/_lib/guru/math/guru_sem.m
+++
@@ -1,6 +0,0 @@
-function sem = guru_sem(data, dim)
-    if ~exist('dim','var')
-        sem = std(data) ./ sqrt(length(data));
-    else
-        sem = std(data, [], dim) ./ sqrt(size(data, dim));
-    end;
